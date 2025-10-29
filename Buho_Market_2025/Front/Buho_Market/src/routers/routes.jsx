811aaf867ef0331e620e3eeab1abf5ff9f1667f1
David-Ventura24 <00089724@uca.edu.sv>
2025-10-28 23:43:53 -0600

    Estilo del login, registrase, recuperar contrasena

--- Buho_Market_2025/Front/Buho_Market/src/routers/routes.jsx
+++ Buho_Market_2025/Front/Buho_Market/src/routers/routes.jsx
@@ -1,14 +1,18 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "../Pages/Login.jsx";   
-import Home from "../Pages/Home.jsx";     
-import Perfil from "../Pages/Perfil.jsx"; 
-
+// src/routers/routes.jsx
+import { Routes, Route } from 'react-router-dom';
+import Login from '../Pages/Login';
+import Registrarse from '../Pages/Registrarse';
+import RecuperearContrasena from '../Pages/RecuperarContrasena';
+ 
 
 export default function MyRoutes() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route paath="/login" element={<Login />} />
+      <Route path="/registrarse" element={<Registrarse />} />
+      <Route path="/recuperarcontrasena" element={<RecuperearContrasena />} />
+ 
+    </Routes>
   );
 }
